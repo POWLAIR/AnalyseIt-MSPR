4e27889e04d8a048ca837d20f2c4dbf4388f8ce3
PowlAIR <paul.claverie@live.fr>
2025-04-28 06:24:53 +0200

    Refactor database connection handling in session.py and db.py
    - Updated to use settings.SQLALCHEMY_DATABASE_URL for database engine creation.
    - Removed the construction of DATABASE_URL from both files for improved consistency and clarity in configuration management.

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,16 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from ..core.config.settings import settings
+from core.config.settings import settings
 
-# Construction de l'URL de la base de données
-DATABASE_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
-
-# Création du moteur SQLAlchemy
 engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,  # Vérifie la connexion avant chaque requête
-    pool_recycle=3600,   # Recycle les connexions après 1 heure
-    echo=False          # Désactive les logs SQL
+    settings.SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    echo=False
 )
 
 # Création de la session factory
